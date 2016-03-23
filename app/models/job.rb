9bf33c852db4ccee8082f07662a8581ad15b2211
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-03-22 17:44:12 -0700

    Added validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,13 @@
 class Job < ActiveRecord::Base
-
+  
   has_many :submissions
   has_many :candidates, through: :submissions
+  accepts_nested_attributes_for :submissions
+
   belongs_to :user
   belongs_to :company
+
   validates :title, presence: true
+  validates :user_id, presence: true
+  validates :company_id, presence: true
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,8 @@
 class Submission < ActiveRecord::Base
+  
+  belongs_to :job
+  belongs_to :candidate
+  validates :user_id, presence: true
+  validates :job_id, presence: true
+  validates :candidate_id, presence: true
 end
