b0cbe59f39b721e8778860549de1778f43b1c314
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-03-22 17:44:02 -0700

    Added validations and enum

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,8 +1,18 @@
 class Candidate < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :status, presence: true
+  
   belongs_to :user
   has_many :submissions
   has_many :jobs, through: :submissions
+  accepts_nested_attributes_for :submissions
+  
+  enum status: {
+    needs_approval: 0,
+    needs_feedback: 1,
+    needs_interview: 2,
+    final_stages: 3
+  }
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :status, presence: true
+  validates :user_id, presence: true
 end
