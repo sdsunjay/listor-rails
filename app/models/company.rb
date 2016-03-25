160387f0003a7093121bdda946e2eea81a44d3f9
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-03-25 02:38:07 -0700

    lots of changes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,12 @@
 class Company < ActiveRecord::Base
 validates :name, presence: true
+    has_many :users
     has_many :jobs
+    has_many :notes, dependent: :destroy  
+    has_many :locations
+    has_many :company_locations
+    has_many :locations, through: :company_locations
+    accepts_nested_attributes_for :company_locations
+
+    # validates :location_id, presence: true
 end
