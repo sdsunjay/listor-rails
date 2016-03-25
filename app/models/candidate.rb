e7956baccacf401369dd83f4e4c50de5a817437b
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-03-25 02:36:53 -0700

    lots of modifications

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -4,7 +4,12 @@ class Candidate < ActiveRecord::Base
   has_many :submissions
   has_many :jobs, through: :submissions
   accepts_nested_attributes_for :submissions
-  
+  has_many :notes, dependent: :destroy  
+
+  has_many :candidate_skills
+  has_many :skills, through: :candidate_skills
+  accepts_nested_attributes_for :candidate_skills
+
   enum status: {
     needs_approval: 0,
     needs_feedback: 1,
@@ -15,4 +20,8 @@ class Candidate < ActiveRecord::Base
   validates :email, presence: true
   validates :status, presence: true
   validates :user_id, presence: true
+
+
+   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
