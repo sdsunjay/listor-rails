6b85cc8a785302fdbff8693c97983fb97fb1f672
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-03-24 23:59:15 -0700

    User belongs to a company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_params, if: :devise_controller?
     def configure_devise_params
         devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit(:name, :email, :password, :password_confirmation)
+        u.permit(:name, :email, :company_id, :password, :password_confirmation)
         end
     end
 
